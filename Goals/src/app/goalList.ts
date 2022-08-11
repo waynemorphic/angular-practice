3819088bcdac43fd330b2e93333ebea70b8cd626
waynemorphic <wayneklyde@gmail.com>
2022-08-11 15:48:26 +0300

    dependency injection, takes goals list from the component to a list file. If there was a backend for the app, this file would store the data instead of the component

---
+++ Goals/src/app/goalList.ts
@@ -0,0 +1,6 @@
+import { Goal } from './goal';
+
+export let goals:Goal[] = [
+    new Goal(1, "Watch The Avengers", "Maybe tomorrow"),
+    new Goal(2, "Watch The Spiderman", "Buy popcorns too")
+  ];
